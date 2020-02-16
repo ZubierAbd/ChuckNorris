6f31b4f68fc82f06dda49df3e90e3e49a37a8e11
Zubier Abdullah <zubierabd@gmail.com>
2020-02-16 10:38:33 -0500

    made changes for chuck norris App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'chucknorris';
+  title = "Chuck Norris App";
+  quote = "";
+  iconUrl = "";
+  url = "https://api.chucknorris.io/jokes/random";
+
+  constructor(private http: HttpClient) {}
+  getQuote() {
+    console.log("Get quote");
+    return this.http.get(this.url).subscribe(res => {
+      console.log(res);
+      this.quote = res["value"];
+      this.iconUrl = res["icon_url"];
+    });
+  }
+
+  ngOnInit() {
+    this.getQuote();
+  }
 }
